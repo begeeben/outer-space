ac1e35f3cb62d4a3563e9ae605972d784924bfee
Yi-Fan Liao <begeeben@gmail.com>
2012-11-14 12:19:05 +0800

    rename files

---
+++ js/OuterSpace.reel.js
@@ -0,0 +1,15 @@
+OuterSpace.reel = function (context, ) {
+
+};
+
+OuterSpace.reel.prototype.start = function () {
+
+};
+
+OuterSpace.reel.prototype.stop = function () {
+
+};
+
+OuterSpace.reel.prototype.bounce = function () {
+
+};
