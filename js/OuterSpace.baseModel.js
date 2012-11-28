f2ff1985cc1d6de285e77d80c393f8a92695aaa1
Yi-Fan Liao <begeeben@gmail.com>
2012-11-28 11:28:22 +0800

    load dog helmet

--- js/OuterSpace.baseModel.js
+++ js/OuterSpace.baseModel.js
@@ -1,406 +1,285 @@
-/**
- * @author begeeben
- */
-
-// var MANX = MANX || {};
-
-// MANX.SlotMachine = {};
-
 OuterSpace.baseModel = function() {
 
-	var scope = this;
-
-	this.scale = 1;
-
-	// animation parameters
-	this.animationFPS = 30;
-	this.transitionFrames = 15;
-
-	// movement model parameters
-	// this.maxSpeed = 275;
-	// this.maxReverseSpeed = -275;
-	// this.frontAcceleration = 600;
-	// this.backAcceleration = 600;
-	// this.frontDecceleration = 600;
-	// this.angularSpeed = 2.5;
-	// rig
-	this.root = new THREE.Object3D();
-
-	this.meshBody = null;
-	// this.meshHelmet = null;
-
-	this.controls = null;
-
-	// textures
-	this.texturesBody = [];
-	// this.skinsWeapon = [];
-	// this.weapons = [];
-	this.currentTexture = undefined;
-
-	//
-	this.onLoadComplete = function() {};
-
-	// internals
-	this.meshes = [];
-	this.animations = {};
-
-	this.loadCounter = 0;
-
-	// internal movement control variables
-	// this.speed = 0;
-	// this.bodyOrientation = 0;
-	// this.walkSpeed = this.maxSpeed;
-	// this.crouchSpeed = this.maxSpeed * 0.5;
-	// internal animation parameters
-	this.activeAnimation = null;
-	this.oldAnimation = null;
-
-	// API
-	this.enableShadows = function(enable) {
-
-		for(var i = 0; i < this.meshes.length; i++) {
-
-			this.meshes[i].castShadow = enable;
-			this.meshes[i].receiveShadow = enable;
-
-		}
-
-	};
-
-	this.setVisible = function(enable) {
-
-		for(var i = 0; i < this.meshes.length; i++) {
-
-			this.meshes[i].visible = enable;
-			this.meshes[i].visible = enable;
-
-		}
-
-	};
-
-	// this.shareParts = function ( original ) {
-	// 	this.animations = original.animations;
-	// 	this.walkSpeed = original.walkSpeed;
-	// 	this.crouchSpeed = original.crouchSpeed;
-	// 	this.skinsBody = original.skinsBody;
-	// 	this.skinsWeapon = original.skinsWeapon;
-	// 	// BODY
-	// 	var mesh = createPart( original.meshBody.geometry, this.skinsBody[ 0 ] );
-	// 	mesh.scale.set( this.scale, this.scale, this.scale );
-	// 	this.root.position.y = original.root.position.y;
-	// 	this.root.add( mesh );
-	// 	this.meshBody = mesh;
-	// 	this.meshes.push( mesh );
-	// 	// WEAPONS
-	// 	for ( var i = 0; i < original.weapons.length; i ++ ) {
-	// 		var meshWeapon = createPart( original.weapons[ i ].geometry, this.skinsWeapon[ i ] );
-	// 		meshWeapon.scale.set( this.scale, this.scale, this.scale );
-	// 		meshWeapon.visible = false;
-	// 		meshWeapon.name = original.weapons[ i ].name;
-	// 		this.root.add( meshWeapon );
-	// 		this.weapons[ i ] = meshWeapon;
-	// 		this.meshWeapon = meshWeapon;
-	// 		this.meshes.push( meshWeapon );
-	// 	}
-	// };
-	this.loadParts = function(config) {
-
-		this.animations = config.animations;
-		// this.walkSpeed = config.walkSpeed;
-		// this.crouchSpeed = config.crouchSpeed;
-		// this.loadCounter = config.weapons.length * 2 + config.textures.length + 1;
-		this.loadCounter = config.textures.length + 1;
-
-		// var weaponsTextures = []
-		// for ( var i = 0; i < config.weapons.length; i ++ ) weaponsTextures[ i ] = config.weapons[ i ][ 1 ];
-		// SKINS
-		this.texturesBody = loadTextures(config.baseUrl + "textures/", config.textures);
-		// this.skinsWeapon = loadTextures( config.baseUrl + "skins/", weaponsTextures );
-		// BODY
-		var loader = new THREE.JSONLoader();
-
-		loader.load(config.baseUrl + config.body, function(geo) {
-
-			geo.computeBoundingBox();
-			// scope.root.position.y = -scope.scale * geo.boundingBox.min.y;
-
-			var mesh = createPart(geo, scope.texturesBody[0]);
-			// var mesh = createPart(geo, new THREE.MeshBasicMaterial());
-			mesh.scale.set(scope.scale, scope.scale, scope.scale);
+  var scope = this;
 
-			scope.root.add(mesh);
+  this.scale = 1;
 
-			scope.meshBody = mesh;
-			scope.meshes.push(mesh);
+  // animation parameters
+  this.animationFPS = 30;
+  this.transitionFrames = 15;
 
-			checkLoadingComplete();
+  // rig
+  this.root = new THREE.Object3D();
 
-		});
+  this.meshBody = null;
+  this.meshHelmet = null;
+  this.controls = null;
 
-		// WEAPONS
-		// var generateCallback = function ( index, name ) {
-		// 	return function( geo ) {
-		// 		var mesh = createPart( geo, scope.skinsWeapon[ index ] );
-		// 		mesh.scale.set( scope.scale, scope.scale, scope.scale );
-		// 		mesh.visible = false;
-		// 		mesh.name = name;
-		// 		scope.root.add( mesh );
-		// 		scope.weapons[ index ] = mesh;
-		// 		scope.meshWeapon = mesh;
-		// 		scope.meshes.push( mesh );
-		// 		checkLoadingComplete();
-		// 	}
-		// }
-		// for ( var i = 0; i < config.weapons.length; i ++ ) {
-		// 	loader.load( config.baseUrl + config.weapons[ i ][ 0 ], generateCallback( i, config.weapons[ i ][ 0 ] ) );
-		// }
-	};
+  // textures
+  this.texturesBody = [];
+  this.texturesHelmet = [];
+  this.helmets = [];
+  this.currentTexture = undefined;
 
-	this.setPlaybackRate = function(rate) {
+  //
+  this.onLoadComplete = function() {};
 
-		if(this.meshBody) this.meshBody.duration = this.meshBody.baseDuration / rate;
-		// if ( this.meshWeapon ) this.meshWeapon.duration = this.meshWeapon.baseDuration / rate;
-	};
+  // internals
+  this.meshes = [];
+  this.animations = {};
 
-	// this.setWireframe = function ( wireframeEnabled ) {
-	// 	if ( wireframeEnabled ) {
-	// 		if ( this.meshBody ) this.meshBody.material = this.meshBody.materialWireframe;
-	// 		if ( this.meshWeapon ) this.meshWeapon.material = this.meshWeapon.materialWireframe;
-	// 	} else {
-	// 		if ( this.meshBody ) this.meshBody.material = this.meshBody.materialTexture;
-	// 		if ( this.meshWeapon ) this.meshWeapon.material = this.meshWeapon.materialTexture;
-	// 	}
-	// };
-	this.setTexture = function(index) {
+  this.loadCounter = 0;
 
-		if(this.meshBody && this.meshBody.material.wireframe === false) {
+  // internal animation parameters
+  this.activeAnimation = null;
+  this.oldAnimation = null;
 
-			this.meshBody.material.map = this.texturesBody[index];
-			this.currentTexture = index;
+  // API
+  this.enableShadows = function(enable) {
 
-		}
+    for(var i = 0; i < this.meshes.length; i++) {
 
-	};
+      this.meshes[i].castShadow = enable;
+      this.meshes[i].receiveShadow = enable;
 
-	// this.setWeapon = function ( index ) {
-	// 	for ( var i = 0; i < this.weapons.length; i ++ ) this.weapons[ i ].visible = false;
-	// 	var activeWeapon = this.weapons[ index ];
-	// 	if ( activeWeapon ) {
-	// 		activeWeapon.visible = true;
-	// 		this.meshWeapon = activeWeapon;
-	// 		if ( this.activeAnimation ) {
-	// 			activeWeapon.playAnimation( this.activeAnimation );
-	// 			this.meshWeapon.setAnimationTime( this.activeAnimation, this.meshBody.getAnimationTime( this.activeAnimation ) );
-	// 		}
-	// 	}
-	// };
-	this.setAnimation = function(animationName) {
+    }
 
-		if(animationName === this.activeAnimation || !animationName) return;
+  };
 
-		if(this.meshBody) {
+  this.setVisible = function(enable) {
 
-			this.meshBody.setAnimationWeight(animationName, 0);
-			this.meshBody.playAnimation(animationName);
+    for(var i = 0; i < this.meshes.length; i++) {
 
-			this.oldAnimation = this.activeAnimation;
-			this.activeAnimation = animationName;
+      this.meshes[i].visible = enable;
+      this.meshes[i].visible = enable;
 
-			this.blendCounter = this.transitionFrames;
+    }
 
-		}
+  };
 
-		// if ( this.meshWeapon ) {
-		// 	this.meshWeapon.setAnimationWeight( animationName, 0 );
-		// 	this.meshWeapon.playAnimation( animationName );
-		// }
-	};
+  this.loadParts = function(config) {
 
-	this.update = function(delta) {
+    this.animations = config.animations;
+    // this.walkSpeed = config.walkSpeed;
+    // this.crouchSpeed = config.crouchSpeed;
+    this.loadCounter = config.helmets.length * 2 + config.textures.length + 1;
+    this.loadCounter = config.textures.length + 1;
 
-		// if(this.controls) this.updateMovementModel(delta);
+    var helmetsTextures = []
+    for ( var i = 0; i < config.helmets.length; i ++ ) helmetsTextures[ i ] = config.helmets[ i ][ 1 ];
+    // SKINS
+    this.texturesBody = loadTextures(config.baseUrl + "textures/", config.textures);
+    this.texturesHelmet = loadTextures( config.baseUrl + "textures/", helmetsTextures );
+    // BODY
+    var loader = new THREE.JSONLoader();
 
-		if(this.animations) {
+    loader.load(config.baseUrl + config.body, function(geo) {
 
-			this.updateBehaviors(delta);
-			this.updateAnimations(delta);
+      geo.computeBoundingBox();
+      // scope.root.position.y = -scope.scale * geo.boundingBox.min.y;
+      var mesh = createPart(geo, scope.texturesBody[0]);
+      // var mesh = createPart(geo, new THREE.MeshBasicMaterial());
+      mesh.scale.set(scope.scale, scope.scale, scope.scale);
 
-		}
+      scope.root.add(mesh);
 
-	};
+      scope.meshBody = mesh;
+      scope.meshes.push(mesh);
 
-	this.updateAnimations = function(delta) {
+      checkLoadingComplete();
 
-		var mix = 1;
+    });
 
-		if(this.blendCounter > 0) {
+    // helmets
+    var generateCallback = function ( index, name ) {
+     return function( geo ) {
+       var mesh = createPart( geo, scope.texturesHelmet[ index ] );
+       mesh.scale.set( scope.scale, scope.scale, scope.scale );
+       mesh.visible = false;
+       mesh.name = name;
+       scope.root.add( mesh );
+       scope.helmets[ index ] = mesh;
+       scope.meshHelmet = mesh;
+       scope.meshes.push( mesh );
+       checkLoadingComplete();
+     }
+    }
+    for ( var i = 0; i < config.helmets.length; i ++ ) {
+     loader.load( config.baseUrl + config.helmets[ i ][ 0 ], generateCallback( i, config.helmets[ i ][ 0 ] ) );
+    }
+  };
 
-			mix = (this.transitionFrames - this.blendCounter) / this.transitionFrames;
-			this.blendCounter -= 1;
+  this.setPlaybackRate = function(rate) {
 
-		}
+    if(this.meshBody) this.meshBody.duration = this.meshBody.baseDuration / rate;
+    if ( this.meshHelmet ) this.meshHelmet.duration = this.meshHelmet.baseDuration / rate;
+  };
 
-		if(this.meshBody) {
+  this.setTexture = function(index) {
 
-			this.meshBody.update(delta);
+    if(this.meshBody && this.meshBody.material.wireframe === false) {
 
-			this.meshBody.setAnimationWeight(this.activeAnimation, mix);
-			this.meshBody.setAnimationWeight(this.oldAnimation, 1 - mix);
+      this.meshBody.material.map = this.texturesBody[index];
+      this.currentTexture = index;
 
-		}
+    }
 
-		// if ( this.meshWeapon ) {
-		// 	this.meshWeapon.update( delta );
-		// 	this.meshWeapon.setAnimationWeight( this.activeAnimation, mix );
-		// 	this.meshWeapon.setAnimationWeight( this.oldAnimation,  1 - mix );
-		// }
-	};
+  };
 
-	this.updateBehaviors = function(delta) {
+  this.setHelmet = function ( index ) {
+   for ( var i = 0; i < this.helmets.length; i ++ ) this.helmets[ i ].visible = false;
+   var activeWeapon = this.helmets[ index ];
+   if ( activeWeapon ) {
+     activeWeapon.visible = true;
+     this.meshHelmet = activeWeapon;
+     if ( this.activeAnimation ) {
+       activeWeapon.playAnimation( this.activeAnimation );
+       this.meshHelmet.setAnimationTime( this.activeAnimation, this.meshBody.getAnimationTime( this.activeAnimation ) );
+     }
+   }
+  };
 
-		var controls = this.controls;
-		var animations = this.animations;
+  this.setAnimation = function(animationName) {
 
-		var moveAnimation, idleAnimation;
+    if(animationName === this.activeAnimation || !animationName) return;
 
-		if (controls.run) {
-			if(this.activeAnimation !== animations["run"]){
-				this.setAnimation(animations["run"]);
-			}
-		}
-		else if (controls.jump) {
-			if(this.activeAnimation !== animations["jump"]){
-				this.setAnimation(animations["jump"]);
-			}
-		}
-		else {
-			if(this.activeAnimation !== animations["stand"]){
-				this.setAnimation(animations["stand"]);
-			}
-		}
+    if(this.meshBody) {
 
-	};
+      this.meshBody.setAnimationWeight(animationName, 0);
+      this.meshBody.playAnimation(animationName);
 
-	this.updateMovementModel = function(delta) {
+      this.oldAnimation = this.activeAnimation;
+      this.activeAnimation = animationName;
 
-		var controls = this.controls;
+      this.blendCounter = this.transitionFrames;
 
-		// speed based on controls
-		if(controls.crouch) this.maxSpeed = this.crouchSpeed;
-		else this.maxSpeed = this.walkSpeed;
+    }
 
-		this.maxReverseSpeed = -this.maxSpeed;
+    if ( this.meshHelmet ) {
+     this.meshHelmet.setAnimationWeight( animationName, 0 );
+     this.meshHelmet.playAnimation( animationName );
+    }
+  };
 
-		if(controls.moveForward) this.speed = THREE.Math.clamp(this.speed + delta * this.frontAcceleration, this.maxReverseSpeed, this.maxSpeed);
-		if(controls.moveBackward) this.speed = THREE.Math.clamp(this.speed - delta * this.backAcceleration, this.maxReverseSpeed, this.maxSpeed);
+  this.update = function(delta) {
 
-		// orientation based on controls
-		// (don't just stand while turning)
-		var dir = 1;
+    // if(this.controls) this.updateMovementModel(delta);
+    if(this.animations) {
 
-		if(controls.moveLeft) {
+      this.updateBehaviors(delta);
+      this.updateAnimations(delta);
 
-			this.bodyOrientation += delta * this.angularSpeed;
-			this.speed = THREE.Math.clamp(this.speed + dir * delta * this.frontAcceleration, this.maxReverseSpeed, this.maxSpeed);
+    }
 
-		}
+  };
 
-		if(controls.moveRight) {
+  this.updateAnimations = function(delta) {
 
-			this.bodyOrientation -= delta * this.angularSpeed;
-			this.speed = THREE.Math.clamp(this.speed + dir * delta * this.frontAcceleration, this.maxReverseSpeed, this.maxSpeed);
+    var mix = 1;
 
-		}
+    if(this.blendCounter > 0) {
 
-		// speed decay
-		if(!(controls.moveForward || controls.moveBackward)) {
+      mix = (this.transitionFrames - this.blendCounter) / this.transitionFrames;
+      this.blendCounter -= 1;
 
-			if(this.speed > 0) {
+    }
 
-				var k = exponentialEaseOut(this.speed / this.maxSpeed);
-				this.speed = THREE.Math.clamp(this.speed - k * delta * this.frontDecceleration, 0, this.maxSpeed);
+    if(this.meshBody) {
 
-			} else {
+      this.meshBody.update(delta);
 
-				var k = exponentialEaseOut(this.speed / this.maxReverseSpeed);
-				this.speed = THREE.Math.clamp(this.speed + k * delta * this.backAcceleration, this.maxReverseSpeed, 0);
+      this.meshBody.setAnimationWeight(this.activeAnimation, mix);
+      this.meshBody.setAnimationWeight(this.oldAnimation, 1 - mix);
 
-			}
+    }
 
-		}
+    if ( this.meshHelmet ) {
+     this.meshHelmet.update( delta );
+     this.meshHelmet.setAnimationWeight( this.activeAnimation, mix );
+     this.meshHelmet.setAnimationWeight( this.oldAnimation,  1 - mix );
+    }
+  };
 
-		// displacement
-		var forwardDelta = this.speed * delta;
+  this.updateBehaviors = function(delta) {
 
-		this.root.position.x += Math.sin(this.bodyOrientation) * forwardDelta;
-		this.root.position.z += Math.cos(this.bodyOrientation) * forwardDelta;
+    var controls = this.controls;
+    var animations = this.animations;
 
-		// steering
-		this.root.rotation.y = this.bodyOrientation;
+    var moveAnimation, idleAnimation;
 
-	};
+    if(controls.run) {
+      if(this.activeAnimation !== animations["run"]) {
+        this.setAnimation(animations["run"]);
+      }
+    } else if(controls.jump) {
+      if(this.activeAnimation !== animations["jump"]) {
+        this.setAnimation(animations["jump"]);
+      }
+    } else {
+      if(this.activeAnimation !== animations["stand"]) {
+        this.setAnimation(animations["stand"]);
+      }
+    }
 
-	// internal helpers
+  };
 
-	function loadTextures(baseUrl, textureUrls) {
+  // internal helpers
 
-		var mapping = new THREE.UVMapping();
-		var textures = [];
+  function loadTextures(baseUrl, textureUrls) {
 
-		for(var i = 0; i < textureUrls.length; i++) {
+    var mapping = new THREE.UVMapping();
+    var textures = [];
 
-			textures[i] = THREE.ImageUtils.loadTexture(baseUrl + textureUrls[i], mapping, checkLoadingComplete);
-			textures[i].name = textureUrls[i];
+    for(var i = 0; i < textureUrls.length; i++) {
 
-		}
+      textures[i] = THREE.ImageUtils.loadTexture(baseUrl + textureUrls[i], mapping, checkLoadingComplete);
+      textures[i].name = textureUrls[i];
 
-		return textures;
+    }
 
-	};
+    return textures;
 
-	function createPart(geometry, textureMap) {
+  };
 
-		geometry.computeMorphNormals();
+  function createPart(geometry, textureMap) {
 
-		// var whiteMap = THREE.ImageUtils.generateDataTexture( 1, 1, new THREE.Color( 0xffffff ) );
-		// var materialWireframe = new THREE.MeshPhongMaterial( { color: 0xffaa00, specular: 0x111111, shininess: 50, wireframe: true, shading: THREE.SmoothShading, map: whiteMap, morphTargets: true, morphNormals: true, perPixel: true, metal: true } );
-		var materialTexture = new THREE.MeshPhongMaterial({
-			color: 0xffffff,
-			specular: 0x111111,
-			shininess: 50,
-			wireframe: false,
-			shading: THREE.SmoothShading,
-			map: textureMap,
-			morphTargets: true,
-			morphNormals: true,
-			perPixel: true,
-			metal: true
-		});
-		materialTexture.wrapAround = true;
+    geometry.computeMorphNormals();
 
-		//
-		var mesh = new THREE.MorphBlendMesh(geometry, materialTexture);
-		mesh.rotation.y = -Math.PI / 2;
+    // var whiteMap = THREE.ImageUtils.generateDataTexture( 1, 1, new THREE.Color( 0xffffff ) );
+    // var materialWireframe = new THREE.MeshPhongMaterial( { color: 0xffaa00, specular: 0x111111, shininess: 50, wireframe: true, shading: THREE.SmoothShading, map: whiteMap, morphTargets: true, morphNormals: true, perPixel: true, metal: true } );
+    var materialTexture = new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      specular: 0x111111,
+      shininess: 50,
+      wireframe: false,
+      shading: THREE.SmoothShading,
+      map: textureMap,
+      morphTargets: true,
+      morphNormals: true,
+      perPixel: true,
+      metal: true
+    });
+    materialTexture.wrapAround = true;
 
-		//
-		mesh.materialTexture = materialTexture;
-		// mesh.materialWireframe = materialWireframe;
-		//
-		mesh.autoCreateAnimations(scope.animationFPS);
+    //
+    var mesh = new THREE.MorphBlendMesh(geometry, materialTexture);
+    mesh.rotation.y = -Math.PI / 2;
 
-		return mesh;
+    //
+    mesh.materialTexture = materialTexture;
+    // mesh.materialWireframe = materialWireframe;
+    //
+    mesh.autoCreateAnimations(scope.animationFPS);
 
-	};
+    return mesh;
 
-	function checkLoadingComplete() {
+  };
 
-		scope.loadCounter -= 1;
-		if(scope.loadCounter === 0) scope.onLoadComplete();
+  function checkLoadingComplete() {
 
-	};
+    scope.loadCounter -= 1;
+    if(scope.loadCounter === 0) scope.onLoadComplete();
 
-	function exponentialEaseOut(k) {
-		return k === 1 ? 1 : -Math.pow(2, -10 * k) + 1;
-	}
+  };
 
 };
--- js/OuterSpace.charactersScene.js
+++ js/OuterSpace.charactersScene.js
@@ -1,24 +1,24 @@
-OuterSpace.charactersScene = function (container) {
+OuterSpace.charactersScene = function(container) {
   this.container = container;
   this.scene = new THREE.Scene();
 
   this.renderer = null;
-  if( Detector.webgl ){
+  if(Detector.webgl) {
     // this.renderer = new THREE.WebGLRenderer();
     this.renderer = new THREE.WebGLRenderer({
-      antialias : true, // to get smoother output
-      preserveDrawingBuffer : true  // to allow screenshot
+      antialias: true,
+      // to get smoother output
+      preserveDrawingBuffer: true // to allow screenshot
     });
     // this.renderer.setClearColorHex( 0xBBBBBB, 1 );
-  // uncomment if webgl is required
-  //}else{
-  // Detector.addGetWebGLMessage();
-  // return true;
+    // uncomment if webgl is required
+    //}else{
+    // Detector.addGetWebGLMessage();
+    // return true;
+  } else {
+    this.renderer = new THREE.CanvasRenderer();
   }
-  else{
-    this.renderer  = new THREE.CanvasRenderer();
-  }
-  this.renderer.setSize( this.container.clientWidth, this.container.clientHeight );
+  this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
   this.container.appendChild(this.renderer.domElement);
 
   this.camera = null;
@@ -36,24 +36,24 @@ OuterSpace.charactersScene = function (container) {
 
 OuterSpace.charactersScene.prototype = {
 
-	constructor: OuterSpace.charactersScene,
+  constructor: OuterSpace.charactersScene,
 
-  _init: function () {
+  _init: function() {
     this._setCamera();
     this._setLights();
     this._addObjects();
     this._animate();
   },
 
-  _setCamera: function () {
+  _setCamera: function() {
     // put a camera in the scene
-    this.camera = new THREE.PerspectiveCamera(45, this.container.clientWidth / this.container.clientHeight, 0.1, 10000 );
+    this.camera = new THREE.PerspectiveCamera(45, this.container.clientWidth / this.container.clientHeight, 0.1, 10000);
     this.camera.position.set(0, 0, 150);
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
     this.scene.add(this.camera);
   },
 
-  _setLights: function () {
+  _setLights: function() {
     // create light
     var pointLight = new THREE.PointLight(0xFFFFFF);
     pointLight.position.x = 0;
@@ -63,7 +63,9 @@ OuterSpace.charactersScene.prototype = {
     this.scene.add(pointLight);
   },
 
-  _addObjects: function () {
+  _addObjects: function() {
+    var that = this;
+    
     this.dog = new OuterSpace.baseModel();
     this.dog.scale = 1;
     this.dog.controls = {
@@ -74,20 +76,27 @@ OuterSpace.charactersScene.prototype = {
       baseUrl: "js/models/dog/",
       body: "dog.js",
       textures: ["dog.png"],
+      helmets: [
+        ["helmet.js", "helmet.png"]
+      ],
       animations: {
         stand: "stand"
       }
     });
 
-    this.dog.root.position.x = -65;
-    this.dog.root.position.y = 12;
-    this.dog.root.position.z = 0;
+    this.dog.onLoadComplete = function() {
+      that.dog.setHelmet(0);
+      that.dog.enableShadows(true);
 
-    this.dog.root.rotation.x = 0.5;
-    this.dog.root.rotation.y = -0.5;
+      that.dog.root.position.x = -65;
+      that.dog.root.position.y = 12;
+      that.dog.root.position.z = 0;
 
-    this.scene.add(this.dog.root);
+      that.dog.root.rotation.x = 0.5;
+      that.dog.root.rotation.y = -0.5;
 
+      that.scene.add(that.dog.root);
+    };
 
     this.boy = new OuterSpace.baseModel();
     this.boy.scale = 1;
@@ -99,83 +108,42 @@ OuterSpace.charactersScene.prototype = {
       baseUrl: "js/models/boy/",
       body: "boy.js",
       textures: ["boy.png"],
+      helmets: [],
       animations: {
         stand: "stand"
       }
     });
 
+    this.boy.enableShadows(true);
     this.boy.root.position.x = 60;
     this.boy.root.position.y = -30;
     this.boy.root.rotation.y = -2.5;
     this.scene.add(this.boy.root);
 
-
-
-    // this.pig = new MANX.SlotMachine.BaseModel();
-    // this.pig.scale = 1;
-    // this.pig.controls = {
-    //   run: false,
-    //   jump: false
-    // };
-    // this.pig.loadParts({
-    //   baseUrl: "js/models/blackpig/",
-    //   body: "blackpig.js",
-    //   textures: ["BlackPig_diff.png"],
-    //   animations: {
-    //     stand: "stand"
-    //   }
-    // });
-
-    // this.pig.root.rotation.y = 2.5;
-
-    // this.scene.add(this.pig.root);
-
-    // for (i=0; i<4; i++) {
-    //    this.objects[i] = new MANX.SlotMachine.BaseModel();
-    // this.objects[i].scale = 1;
-    // this.objects[i].controls = {
-    //   run: false,
-    //   jump: false
-    // };
-    // this.objects[i].loadParts({
-    //   baseUrl: "js/models/boy/",
-    //   body: "boy.js",
-    //   textures: ["boy.png"],
-    //   animations: {
-    //     stand: "stand"
-    //   }
-    // });
-
-    // this.scene.add(this.objects[i].root);
-    // }
   },
 
-  _animate: function () {
+  _animate: function() {
     requestAnimationFrame(this._animate.bind(this));
     this._render();
 
-    if (this.stats) {
+    if(this.stats) {
       this.stats.update();
     }
   },
 
-  _render: function () {
+  _render: function() {
     var delta = this.clock.getDelta();
     this.dog.update(delta);
     this.boy.update(delta);
-    // this.pig.update(delta);
-    // for (i=0; i<4; i++) {      
-    //   this.objects[i].update(delta);
-    // }
     this.renderer.render(this.scene, this.camera);
   },
 
-  showStats: function () {
+  showStats: function() {
     // add Stats.js - https://github.com/mrdoob/stats.js
     this.stats = new Stats();
     this.stats.domElement.style.position = 'absolute';
     this.stats.domElement.style.bottom = '0px';
-    document.body.appendChild( this.stats.domElement );
+    document.body.appendChild(this.stats.domElement);
   }
 
 };
